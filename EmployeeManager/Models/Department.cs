53da248c811e2740068554ede9496a7792c78912
Hope Ndudim <59125945+ndudimhope@users.noreply.github.com>
2021-10-19 00:18:34 +0100

    Add project files.

---
+++ EmployeeManager/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeManager.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+
+        public DeptNameList DepartmentName { get; set; }
+
+        public List<Employee> Employees { get; set; }
+
+    }
+}
---
+++ EmployeeManager/Models/Enums/DeptNameList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager.Models.Enums
+{
+    public enum DeptNameList
+    {
+        HR,
+        Accounts,
+        IT,
+        CustomerService,
+        Admin
+
+    }
+}
